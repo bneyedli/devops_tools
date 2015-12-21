bb74993de0efef1ea82908fd6b569be257c0c59d
Bryan <bryan@trinimbus.com>
2015-12-21 14:25:07 -0800

    Error handling and stuff

--- gentoo/install.sh
+++ gentoo/install.sh
@@ -12,12 +12,16 @@ die () {
   exit "${2}"
 }
 
-[[ -f ./install.sh ]] || die "Please run from the same directory as project files"
-(( UID == 0 )) || die "Please run as root"
+[[ -f ./install.sh ]] || die "Please run from the same directory as project files" '1'
+(( UID == 0 )) || die "Please run as root" '1'
 
 for file in *.sh
 do
   [[ ${file} == install.sh ]] && continue
-  cp ${file} ${INSTALL_DIR}/$(${BASE} ${file} .sh)
-  [[ ! -x ${INSTALL_DIR}/$(${BASE} ${file} .sh) ]] && ${CHMOD} 755 ${INSTALL_DIR}/$(${BASE} ${file} .sh)
+  cp ${file} ${INSTALL_DIR}/$(${BASE} ${file} .sh) || die "Could not copy ${file} to ${INSTALL_DIR}" '1'
+  if [[ ! -x ${INSTALL_DIR}/$(${BASE} ${file} .sh) ]]
+  then
+    ${CHMOD} 755 ${INSTALL_DIR}/$(${BASE} ${file} .sh) || die "Could not chmod ${file}"
+  fi
 done
+die 'fin.' '0'

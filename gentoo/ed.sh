c9355631cc2631e6b671dd887926cb6b045dc6e8
Bryan <bryan@trinimbus.com>
2015-12-21 16:39:41 -0800

    Input sanitization

--- gentoo/ed.sh
+++ gentoo/ed.sh
@@ -118,10 +118,13 @@ then
   fi
 
   runGit 'add' "${TARGET}"
+
   echo -n "Commit message -> "
   read commitMessage
+
   [[ ${commitMessage} =~ [a-zA-Z0-9].* ]] || die "Weirdo characters: ${commitMessage}, try again" '1'
 
+  #Commit the things
   runGit 'commit' "${commitMessage}"
 fi
 

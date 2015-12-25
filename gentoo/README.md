2eb97a7231a5f13eaf404a9cd2b343b808843a34
Bryan <bryan@trinimbus.com>
2015-12-25 15:33:03 -0800

    unbroke

--- gentoo/README.md
+++ gentoo/README.md
@@ -1,6 +1,6 @@
 # Gentooling
 Script | Name | Description | Checksums
 ------ | ---- | ----------- | ---------
-mm.sh | Merge Master | emerge half-thought wrapper, mount & unmount tmpfs, use genlop. | MD5: 63407e771aa7d463d962c8488edb0c4c
+mm.sh | Merge Master | emerge half-thought wrapper, mount & unmount tmpfs, use genlop. | MD5: 5d04503f3ea03c8cc573adab04f444e0
 ed.sh | Git Wrapper for your favorite editor | edit file, commit if changed, push if needful. | MD5: 3a95bfc96aabaedbca0df4aa7b71587f
 install.sh | Crappy Installer(tm) | Install these here files. | MD5: f5a75df6d83f93ae24e5ba397cbcac13
--- gentoo/mm.sh
+++ gentoo/mm.sh
@@ -4,17 +4,30 @@ set -u -o pipefail
 
 PORTAGE_TMP=/var/tmp/portage/
 PRETEND=0
+SCRIPT_OUT=$(mktemp)
+SCRIPT_FLAGS="-a ${SCRIPT_OUT} -q -c"
 ARGS=""
+
+die () {
+  echo "$1"
+  [[ -f ${SCRIPT_OUT} ]] && rm ${SCRIPT_OUT}
+  checkMount '/var/tmp/portage/' && sudo /bin/umount /var/tmp/portage/
+  exit "$2"
+}
+
+checkMount () {
+  /bin/mount | /bin/grep ${1}
+  return "$?"
+}
 
 #Need 1 arg
 if (( ${#@} < 1 ))
 then
-  echo "Specify a package to merge"
-  exit 1
+  die "Specify a package to merge" '1'
 elif [[ ${1} =~ ^- ]]
 then
   echo "Params supplied: $1"
-  (( ${#@} < 2 )) && echo "Please specify a package" && exit 1
+  (( ${#@} < 2 )) && die 'Please specify a package' '1'
   PACKAGE=$2
   ARGS=$1
   [[ ${ARGS} =~ "p" ]] && PRETEND=1
@@ -23,25 +36,25 @@ else
 fi
 
 #Check if portage is mounted
-( mount|grep /var/tmp/portage/ )
+checkMount '/var/tmp/portage/'
 if (( $? > 0 )) 
 then
   if (( PRETEND == 0 ))
   then
     echo "Mounting portage tmpfs"
-    sudo /bin/mount /var/tmp/portage/ || exit 1
+    sudo /bin/mount /var/tmp/portage/ || die 'Unable to mount tmpfs' '1'
   fi
 fi
 
 if (( PRETEND == 1 ))
 then
   sudo /usr/bin/emerge ${ARGS} ${PACKAGE}
-  exit 0
+  die 'fin' '0'
 elif [[ -z ${ARGS} ]]
 then
-  ( script -q -c "sudo /usr/bin/emerge ${PACKAGE}" &> /dev/null ) &
+  ( script ${SCRIPT_FLAGS} "sudo /usr/bin/emerge ${PACKAGE}" &> /dev/null ) &
 else
-  ( script -q -c "sudo /usr/bin/emerge ${ARGS} ${PACKAGE}" &> /dev/null ) &
+  ( script ${SCRIPT_FLAGS} "sudo /usr/bin/emerge ${ARGS} ${PACKAGE}" &> /dev/null ) &
 fi
 
 EMERGE_PID=$!
@@ -50,8 +63,9 @@ PRINT_STRING="Forked..."
 
 while (( $? == 0 ))
 do 
+  echo "Merging..."
   echo "${PRINT_STRING}"
-  EMERGE_STAT=$(genlop -c -q -i -t | egrep -v -e '!!!' -e 'see manpage') #|sed 's/$/ /'|tr -d '\n')
+  EMERGE_STAT=$(genlop -c -q -i -t | egrep -v -e '!!!' -e 'see manpage' | sed 's/$/ /'|tr -d '\n')
   [[ -n ${EMERGE_STAT} ]] && PRINT_STRING=${EMERGE_STAT}
   sleep 1
   clear
@@ -61,12 +75,6 @@ done
 wait ${EMERGE_PID}
 EMERGE_STATUS=$?
 
-cat typescript
+cat ${SCRIPT_OUT}
 
-if (( ! EMERGE_STATUS  == 0 )) 
-then
-  echo "Emerge returned: ${EMERGE_STATUS}"
-  exit 2
-fi
-
-sudo /bin/umount /var/tmp/portage/
+(( ! EMERGE_STATUS  == 0 )) && die "Emerge returned: ${EMERGE_STATUS}" "${EMERGE_STATUS}"
